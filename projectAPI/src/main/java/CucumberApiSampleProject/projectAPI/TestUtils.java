f6fcbef2fe4e0230ae042a7a9521d349c1e2c242
Sanchit Kumar <sanchitkumar29@gmail.com>
2020-12-10 16:45:19 +0530

    Corrected the HTTP request

--- projectAPI/src/main/java/CucumberApiSampleProject/projectAPI/TestUtils.java
+++ projectAPI/src/main/java/CucumberApiSampleProject/projectAPI/TestUtils.java
@@ -1,26 +1,15 @@
 package CucumberApiSampleProject.projectAPI;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-import javax.net.ssl.HttpsURLConnection;
-
-import org.apache.http.HttpConnection;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
-
-import io.restassured.internal.http.HttpResponseContentTypeFinder;
-import io.restassured.response.Response;
 
 public class TestUtils {
 
@@ -30,11 +19,11 @@ public class TestUtils {
 	//
 	// }
 	// static HttpResponse result = "";
-	public static HttpResponse sendPOST(String url) throws IOException {
+	public static Map<String,String> sendPOST(String url) throws IOException {
 		// TODO Auto-generated method stub
-		CloseableHttpClient httpclient = null;
 
 		HttpPost post = new HttpPost(url);
+		post.addHeader("Content-Type", "application/json");
 
 		// List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
 		// urlParameters.add(new BasicNameValuePair("email", "eve.holt@reqres.in"));
@@ -45,16 +34,16 @@ public class TestUtils {
 		json.append("\"email\":\"eve.holt@reqres.in\",");
 		json.append("\"password\":\"cityslicka\"");
 		json.append("}");
-
+		System.out.println(json.toString());
 		post.setEntity(new StringEntity(json.toString()));
 
-		try {
-			httpclient = HttpClients.createDefault();
-			CloseableHttpResponse response = httpclient.execute(post);
-			return response;
-
-		} finally {
-			httpclient.close();
+		try (CloseableHttpClient httpClient = HttpClients.createDefault();
+				CloseableHttpResponse response = httpClient.execute(post)) {
+			String result = EntityUtils.toString(response.getEntity());
+			Map<String,String> responseMap = new HashMap<>();
+			responseMap.put("responseBody", result);
+			responseMap.put("statusCode", Integer.toString(response.getStatusLine().getStatusCode()));
+			return responseMap;
 		}
 	}
 
--- projectAPI/src/test/java/com/qa/stepdefinitions/LoginSteps.java
+++ projectAPI/src/test/java/com/qa/stepdefinitions/LoginSteps.java
@@ -1,13 +1,10 @@
 package com.qa.stepdefinitions;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
-import java.io.IOException;
+import java.util.Map;
 
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.util.EntityUtils;
-import org.junit.Assert;
 
 import CucumberApiSampleProject.projectAPI.TestUtils;
 import io.cucumber.java.en.Given;
@@ -17,42 +14,46 @@ import io.cucumber.java.en.When;
 public class LoginSteps {
 	private TestUtils testutils;
 	private CloseableHttpResponse responseEntity;
+	private Map<String, String> responseMap;
+
 	public LoginSteps() {
-		 testutils = new TestUtils();
+		testutils = new TestUtils();
 	}
 
 	@Given("User has the valid API url")
 	public void user_has_the_valid_api_url() {
 		// Write code here that turns the phrase above into concrete actions
-		
-		//Assert.assertTrue(testutils.validurl("https://google.com"));
+
+		// Assert.assertTrue(testutils.validurl("https://google.com"));
 	}
 
 	@When("User hit the api with valid credentials")
 	public void user_hit_the_api_with_valid_credentials() throws Exception {
-		// Write code here that turns the phrase above into concrete actions
-		responseEntity= (CloseableHttpResponse) TestUtils.sendPOST("https://reqres.in/api/login");
-		//responseEntity.getStatusLine().getStatusCode();
-		//System.out.println(responseEntity);
-		String result = EntityUtils.toString(responseEntity.getEntity());
-        System.out.println(result);
-		
-		
-		
+		responseMap = TestUtils.sendPOST("https://reqres.in/api/login");
+		System.out.println(responseMap.get("responseBody"));
+
 	}
 
 	@Then("user get the status code as {string}")
-	public void user_get_the_status_code_as(String statusCode) {
-		// Write code here that turns the phrase above into concrete actions
-		//int actual = responseEntity.getStatusLine().getStatusCode();
-		assertEquals(200, responseEntity.getStatusLine().getStatusCode());
+	public void user_get_the_status_code_as(String expectedStatusCode) {
+		String actualCode = responseMap.get("statusCode");
+		System.out.println(actualCode);
+		assertEquals(expectedStatusCode, actualCode);
+
 	}
 
 	@Then("user get the token in the response")
 	public void user_get_the_token_in_the_response() {
-		// Write code here that turns the phrase above into concrete actions
-	
-	
+		// get responseBody from responseMap
+		
+		// Convert the responseBody using ObjectMapper into map
+		
+		// map.get("token")
+		
+		// assertions: token not null, token not empty, token length = <length>
+//		assertNotNull(token);
+//		assertFalse(token.isEmpty())
+//		assertEquals(<expected length value in int>, token.length())
 
 	}
 }
